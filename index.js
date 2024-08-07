f0517ad3254319e773604ba2fd398d9a899a8a4b
ThePromiseIncarnate <ThePromiseIncarnate@gmail.com>
2024-08-07 18:29:10 -0500

    First Day Complete

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 console.log("Hello from index.js");
-myName = "Julie"; 
+myName = "Joshua"; 
 
 if (myName.length > 7) {
     console.log("will marry late in life")
+}
+
+if (myName.length < 5) {
+    console.log("will marry within a year")
+}
+
+if (5 <= myName.length <= 7) {
+    console.log("will encounter a once-in-a-lifetime opurtunity")
+}
+
+if (myName.charAt(0) === "R") {
+    console.log("will be rich")
+}
+
+if (myName.match("i")) {
+    console.log("will fall in love this week")
 }
