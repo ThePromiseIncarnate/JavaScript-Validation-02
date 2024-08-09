f6d001ef6fb2f5e10b6ed590c8bf496805d3fd07
ThePromiseIncarnate <ThePromiseIncarnate@gmail.com>
2024-08-08 19:29:44 -0500

    Assignment Complete

--- index-2.js
+++ index-2.js
@@ -20,12 +20,56 @@ function hasLongName(firstName) {
     }
 }
 
-function tellFortune(input) {
-    let n = hasLongName(input);
-    if (n) {
-        console.log("You will marry late in life");
+function hasShortName(firstName) {
+    let nameLength = firstName.length;
+    if (nameLength < 5) {
+        return true
     } else {
-        console.log("You will not marry late in life");
+        return false
+    }
+}
+
+function hasMediumName(firstName) {
+    let nameLength = firstName.length;
+    if (5 <= nameLength <= 7) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function startsWithR(firstName) {
+    return firstName.charAt(0) === "R"
+}
+
+function hasAnI(firstName) {
+    return firstName.includes("i");
+}
+
+function tellFortune(input) {
+    let m = hasLongName(input);
+    if (m) {
+        console.log(`${input} will marry late in life`);
+    } 
+
+    let n = hasShortName(input);
+    if (n) {
+        console.log(`${input} will marry within a year`)
+    }
+
+    let o = hasMediumName(input);
+    if (o) {
+        console.log(`${input} will encounter a once-in-a-lifetime opurtunity`)
+    }
+
+    let p = startsWithR(input);
+    if (p) {
+        console.log(`${input} will be rich`)
+    }
+
+    let q = hasAnI(input);
+    if (q) {
+        console.log(`${input} will fall in love this week`)
     }
 }
 
